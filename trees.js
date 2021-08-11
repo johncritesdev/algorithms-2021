41300284978d78bfa12f26dff12e021b0ed267a6
John Crites <johncritesdev@gmail.com>
2021-08-11 15:29:51 -0400

    ...

---
+++ trees.js
@@ -0,0 +1,34 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.children = [];
+  }
+
+  add(data) {
+    this.children.push(new Node(data));
+  }
+
+  remove(data) {
+    this.children.filter(node => {
+      return node.data !== data;
+    });
+  }
+}
+
+class Trees {
+  constructor() {
+    this.root = null;
+  }
+
+
+}
+//node test
+// const n = new Node('a');
+// n.add('b')
+// console.log(n)
+
+//tree test
+const node = new Node(10);
+const tree = new Trees();
+tree.root = node;
+console.log(tree);
