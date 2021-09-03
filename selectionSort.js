29b07c332bb05682dd7a91817b9dd5b647165928
John Crites <johncritesdev@gmail.com>
2021-09-03 14:24:32 -0700

    ...

---
+++ selectionSort.js
@@ -0,0 +1,22 @@
+const selectionSort = arr => {
+  for (let i = 0; i < arr.length; i++) {
+    let indexOfMin = i;
+
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[indexOfMin]) {
+        indexOfMin = j;
+      }
+    }
+
+    if (indexOfMin !== i) {
+      let lesser = arr[indexOfMin];
+      arr[indexOfMin] = arr[i];
+      arr[i] = lesser;
+    }
+  }
+  return arr;
+};
+
+const result = selectionSort([10, 0, 97, -30, 5]);
+console.log(result);
+
